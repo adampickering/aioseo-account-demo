0f6bba3d4b93e6b729ec179be39bae6b8359e227
Adam Pickering <adam@rocketgenius.com>
2026-04-09 15:37:19 -0400

    polish: remove italic from acknowledgement line

--- src/views/cancel/Step5Confirmation.vue
+++ src/views/cancel/Step5Confirmation.vue
@@ -22,7 +22,7 @@ function copyCode() {
 			<p class="text-body-lg mt-3 max-w-[480px] mx-auto">
 				Your subscription has been cancelled. Thanks for being a customer, and we hope to see you again.
 			</p>
-			<p class="text-body-lg italic mt-2 max-w-[480px] mx-auto">
+			<p class="text-body-lg mt-2 max-w-[480px] mx-auto">
 				{{ acknowledgement }}
 			</p>
 		</div>
